fb2c982fcaa0c34a4cbfc3add8dc62af3b0f79cd
Ensuo <haradayasuo2010@gmail.com>
2022-07-10 12:49:20 -0300

    Added new painting modes, reset button, erase option, and updated style.css and added new divs

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-var grid = document.getElementById("grid");
-var grid_size = 16;
-var btn_resize = document.getElementById("btn-resize");
+let grid = document.getElementById("grid");
+let grid_size = 16;
+let mode = 'blackmode';
 
-function updateSquareColor(square){
-    if(square.classList.contains('active')){
-        square.classList.remove('active');
-    }else{
-        square.classList.add('active');
-    }
+let btn_resize = document.getElementById("btn-resize");
+btn_resize.addEventListener('click', updateGrid);
+
+let btn_reset = document.getElementById("btn-reset");
+btn_reset.addEventListener('click', resetGrid);
+
+let btn_blackmode = document.getElementById("blackmode");
+btn_blackmode.addEventListener('click', function(){
+    mode = "blackmode";
+});
+
+let btn_rainbowmode = document.getElementById("rainbowmode");
+btn_rainbowmode.addEventListener('click', function(){
+    mode = "rainbowmode";
+});
+
+let btn_erasermode = document.getElementById("erasermode");
+btn_erasermode.addEventListener('click', function(){
+    mode = "erasermode";
+});
+
+if(grid_size > 0){
+    createGrid(grid_size);
 }
 
-/*function updateColor(color){
-    switch(color.className){
-        case 'red':
-            document.querySelector
+function setCurrentmode(curMode){
+    mode = curMode
+}
+
+function updateSquareColor(e){
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    if(mode ==='blackmode'){
+        e.target.classList.add('active');
+        e.target.style.backgroundColor = '#000000';
+    }else if(mode === 'rainbowmode'){
+        e.target.classList.add('active');
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }else if(mode === 'erasermode'){
+        e.target.style.backgroundColor = 'rgb(190, 189, 189)';
+        if(e.target.classList.contains('active')){
+            e.target.classList.remove('active');
+        }
     }
-}*/
+    
+}
 
 function createGrid(size){
     for(var i = 0; i < size; i++){
@@ -33,7 +68,7 @@ function createGrid(size){
     }
 
     document.querySelectorAll(".square").forEach(square => {
-        square.addEventListener("click", () =>  updateSquareColor(square));
+        square.addEventListener("mousedown", updateSquareColor);
     });
 }
 
@@ -47,13 +82,8 @@ function updateGrid(){
     }
 }
 
-btn_resize.addEventListener('click', updateGrid);
-
-/*document.querySelectorAll("colorList").forEach(color => {
-    color.addEventListener("click", () => updateColor(color));
-});*/
-
-if(grid_size > 0){
+function resetGrid(mode){
+    grid.innerHTML = "";
     createGrid(grid_size);
 }
 
